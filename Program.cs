d6ca7584fe1fa31bdd273f6306201d5544961fa2
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-05-19 12:31:50 -0300

    adicionarVoid

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
 ﻿// Escreva uma função que calcule o salário de um funcionário a partir salário-hora base, o total de horas trabalhadas e número de horas-extras executadas. Considere que cada hora-extra é paga com acréscimo de 40%.
+using System;
+using System.Net.WebSockets;
 
-double salarioHoraBase, totalHorasTrabalhadas, numHorasExtras, acrescimo, valorTotalcomacrescimo, vlorPagoTotal;
+namespace HorasExtras
+{
+    class Program {
+        static void Main(string[] args)
+        {
+            double salarioHoraBase, totalHorasTrabalhadas, numHorasExtras;
 
-Console.WriteLine("bem vindo ao calculador de Horas extras ");
-Console.Write("Qual o seu Salario/h base: ");
-salarioHoraBase = Convert.ToDouble(Console.ReadLine());
-Console.Write("Qual o total de horas trabalhadas: ");
-totalHorasTrabalhadas = Convert.ToDouble(Console.ReadLine());
-Console.Write("Digite a quantidade de horas extras: ");
-numHorasExtras = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("bem vindo ao calculador de Horas extras ");
+            Console.Write("Qual o seu Salario/h base: ");
+            salarioHoraBase = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Qual o total de horas trabalhadas: ");
+            totalHorasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a quantidade de horas extras: ");
+            numHorasExtras = Convert.ToDouble(Console.ReadLine());
 
-vlorPagoTotal = salarioHoraBase * totalHorasTrabalhadas;
-acrescimo = (salarioHoraBase * numHorasExtras) * 1.40;
+            // Chamada da função para calcular o salário total
+            double valorTotalComAcrescimo = SalarioCalculo(salarioHoraBase, totalHorasTrabalhadas, numHorasExtras);
 
-valorTotalcomacrescimo = vlorPagoTotal + acrescimo;
+            Console.WriteLine($"O valor total a ser pago é: R$ {valorTotalComAcrescimo:F2}");
 
-Console.Write(valorTotalcomacrescimo);
+
+        }
+        static double SalarioCalculo(double salarioHoraBase, double totalHorasTrabalhadas, double numHorasExtras)
+        {
+            double vlHorasnormais = salarioHoraBase * totalHorasTrabalhadas;
+            double vlHorasExtras = salarioHoraBase * numHorasExtras * 1.40;
+            double vlorPagoTotal = vlHorasExtras + vlHorasnormais;
+            return vlorPagoTotal;
+        }   
+    }    
+}
+
