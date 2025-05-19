8bc558e605c0fc014c2d2131e1552ca7959602f2
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-05-18 21:20:18 -0300

    horasextras

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Escreva uma função que calcule o salário de um funcionário a partir salário-hora base, o total de horas trabalhadas e número de horas-extras executadas. Considere que cada hora-extra é paga com acréscimo de 40%.
+
+double salarioHoraBase, totalHorasTrabalhadas, numHorasExtras, acrescimo, valorTotalcomacrescimo, vlorPagoTotal;
+
+Console.WriteLine("bem vindo ao calculador de Horas extras ");
+Console.Write("Qual o seu Salario/h base: ");
+salarioHoraBase = Convert.ToDouble(Console.ReadLine());
+Console.Write("Qual o total de horas trabalhadas: ");
+totalHorasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a quantidade de horas extras: ");
+numHorasExtras = Convert.ToDouble(Console.ReadLine());
+
+vlorPagoTotal = salarioHoraBase * totalHorasTrabalhadas;
+acrescimo = (salarioHoraBase * numHorasExtras) * 1.40;
+
+valorTotalcomacrescimo = vlorPagoTotal + acrescimo;
+
+Console.Write(valorTotalcomacrescimo);
